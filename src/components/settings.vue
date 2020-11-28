460c311abc97a12d5d880e9d6ad3915b3f6bad8d
Rishu Kumar <rishukr06@gmail.com>
2020-11-28 16:11:04 +0530

    feat(pagination): add pagination

--- src/components/settings.vue
+++ src/components/settings.vue
@@ -1,10 +1,17 @@
 <template>
-  <select @change="addNewLimit()" v-model="newLimit">
-    <option value="10"> 10 </option>
-    <option value="25"> 25 </option>
-    <option value="50"> 50 </option>
-    <option value="100"> 100 </option>
-  </select>
+  <div>
+    <select @change="addNewLimit()" v-model="newLimit">
+      <option value="10"> 10 </option>
+      <option value="25"> 25 </option>
+      <option value="50"> 50 </option>
+      <option value="100"> 100 </option>
+    </select>
+    <br/>
+    <button @click="firstPage()" :disabled=isDisabledFirstPrevious>First</button>&nbsp;
+    <button @click="previousPage()" :disabled=isDisabledFirstPrevious> ← Previous </button>&nbsp;
+    <button @click="nextPage()" :disabled=isDisabledNextLast> Next → </button>&nbsp;
+    <button @click="lastPage()" :disabled=isDisabledNextLast> Last </button>
+  </div>
 </template>
 
 <script>
@@ -18,13 +25,43 @@ name: "settings",
     }
   },
   computed : {
-    ...mapState('coins', ['limit'])
+    ...mapState('coins', ['limit','offset','totalPages','page']),
+
+    isDisabledFirstPrevious(){
+      if(this.offset === 0 ) {
+        return true
+      } else {
+        return false
+      }
+    },
+
+    isDisabledNextLast(){
+      if(this.page === this.totalPages) {
+        return true
+      } else {
+        return false
+      }
+    },
   },
+
   methods: {
     addNewLimit() {
       this.$store.dispatch('coins/setLimit', this.newLimit)
-    }
+    },
+    firstPage(){
+      this.$store.dispatch('coins/firstPage')
+    },
+    previousPage(){
+      this.$store.dispatch('coins/previousPage')
+    },
+    nextPage(){
+      this.$store.dispatch('coins/nextPage')
+    },
+    lastPage(){
+      this.$store.dispatch('coins/lastPage')
+    },
   },
+
 }
 </script>
 
--- src/store/coins.js
+++ src/store/coins.js
@@ -5,15 +5,31 @@ const coins = {
         coins : [],
         limit : 10,
         offset : 0,
+        page : 2,
+        totalCoins: 0,
+        totalPages : 0,
+
         apiUrl : 'https://api.coinranking.com/v1/public/coins/',
     },
     mutations: {
         SET_COINS(state, coins) {
             state.coins = coins
         },
-        SET_LIMIT(state,limit) {
+        SET_LIMIT(state, limit) {
             state.limit = limit
         },
+        SET_OFFSET(state, offset) {
+            state.offset = offset
+        },
+        SET_TOTAL_COINS(state, totalCoins) {
+            state.totalCoins = totalCoins
+        },
+        SET_TOTAL_PAGES(state, totalPages) {
+            state.totalPages = totalPages
+        },
+        SET_PAGE(state, page) {
+            state.page = page
+        }
     },
     actions: {
         loadCoins({commit}) {
@@ -21,11 +37,52 @@ const coins = {
                 .get(`${coins.state.apiUrl}?limit=${coins.state.limit}&offset=${coins.state.offset}`)
                 .then(res => {
                     commit('SET_COINS', res.data.data.coins)
+                    commit('SET_TOTAL_COINS', res.data.data.stats.total)
+                    let totalCoins = coins.state.totalCoins
+                    let limit = coins.state.limit
+                    let totalNumberOfPages = Math.ceil(totalCoins/limit)
+                    commit('SET_TOTAL_PAGES', totalNumberOfPages)
                 })
                 .catch(error => console.log(error))
         },
         setLimit({commit}, limit) {
             commit('SET_LIMIT', limit)
+            coins.actions.loadCoins({commit})
+        },
+        firstPage({commit}) {
+            commit('SET_OFFSET', 0)
+            commit('SET_PAGE', 2)
+            coins.actions.loadCoins({commit})
+        },
+        previousPage({commit}) {
+            let currPage = coins.state.page;
+            let limit = coins.state.limit;
+            if (currPage > 2){
+                let currOffset = coins.state.offset;
+                let data = currOffset - limit;
+                commit('SET_OFFSET', data)
+                commit('SET_PAGE', currPage-1)
+                coins.actions.loadCoins({commit})
+            }
+        },
+        nextPage({commit}) {
+            let currPage = coins.state.page;
+            let limit = coins.state.limit;
+            let lastPage = coins.state.totalPages;
+            if (currPage < lastPage) {
+                let data = (currPage - 1) * limit;
+                commit('SET_OFFSET', data)
+                commit('SET_PAGE', currPage+1)
+                coins.actions.loadCoins({commit})
+            }
+        },
+        lastPage({commit}) {
+            let totalCoins = coins.state.totalCoins
+            let limit = coins.state.limit
+            let totalNumberOfPages = coins.state.totalPages
+            let offset = totalCoins - limit
+            commit('SET_OFFSET', offset)
+            commit('SET_PAGE', totalNumberOfPages)
             coins.actions.loadCoins({commit})
         },
     }
